d2c67161fd4e2a77b4c92d971d92297df1f07218
cirrina <david.lindgren@med.lu.se>
2020-11-28 11:40:48 +0100

    added dots

---
+++ dotfiles/init.el
@@ -0,0 +1,25 @@
+(setq mac-option-key-is-meta t)
+(setq mac-right-option-modifier nil)
+
+(load-theme 'misterioso)
+(global-set-key (kbd "C-x C-a") 'beginning-of-line)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(add-to-list 'auto-mode-alist '("\\.nf\\'" . groovy-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(groovy-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
